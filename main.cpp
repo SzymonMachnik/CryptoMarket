334e12d627a457a2fab9dd30d0bf66104f0d98b0
Szymon Machnik <szymonmachnik07@gmail.com>
2024-10-18 21:19:54 +0200

    edu 02

--- main.cpp
+++ main.cpp
@@ -1,64 +1,64 @@
+
 #include <iostream>
 #include <curl/curl.h>
-#include "./include/json.hpp"
+#include <vector>
+#include <stdlib.h>
+#include <string>
 
 using namespace std;
 
+class memoryClass {
+  private:
+    vector<char> memory; 
+  public:
+    size_t writeMemory(void* contents, size_t size, size_t nmemb, void* userp) {
+      size_t realsize = size * nmemb;
+      memory.insert(memory.end(), (char*)contents, (char*)contents + realsize);
 
-// Funkcja callback do zapisywania odpowiedzi HTTP
-static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) {
-    size_t newLength = size * nmemb;
-    s->append((char*)contents, newLength);
-    return newLength;
-}
-
-// Funkcja do pobierania ceny BTC
-std::string getBTCPrice() {
-    CURL* curl;
-    CURLcode res;
-    std::string readBuffer;
-
-    curl = curl_easy_init();
-    if(curl) {
-        // Ustawienie URL do API CoinGecko
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd");
-        //curl_easy_setopt(curl, CURLOPT_URL, "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);  // Wyłączenie weryfikacji SSL (dla celów testowych)
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        // Ustawienie callbacka do odbierania danych
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        
-        // Wykonanie żądania HTTP
-        res = curl_easy_perform(curl);
-
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            cout << "Error: " << curl_easy_strerror(res) << endl;
-            curl_easy_cleanup(curl);
-            return "";
-        }
-        // Sprawdzanie, czy zapytanie się powiodło
-        if(res != CURLE_OK) {
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-        }
-
-        // Czyszczenie
-        curl_easy_cleanup(curl);
+      return realsize;
     }
-
-    return readBuffer;
-}
+    
+    string returnMemoryAsString() {
+      cout << "Rerurn memory called" << endl;
+      return string(memory.begin(), memory.end());
+    }
+};
 
 int main() {
-    std::string priceData = getBTCPrice();
-    
-    // Parsowanie JSON z odpowiedzi
-    auto jsonData = nlohmann::json::parse(priceData);
-    double btcPrice = jsonData["bitcoin"]["usd"];
 
-    // Wyświetlenie ceny BTC
-    std::cout << "Cena Bitcoin (BTC) w USD: " << btcPrice << std::endl;
+  CURL *curl;
+  CURLcode result;
+  memoryClass chunk;
 
-    return 0;
+  curl = curl_easy_init();
+
+  if (curl == NULL) {
+    cout << "ERROR: HTTP request failed" << endl;
+    return 1;
+  }
+
+  curl_easy_setopt(curl, CURLOPT_URL, "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+  
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &chunk.writeMemory);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk); 
+
+  cout << "TEST 2" << endl;
+  result = curl_easy_perform(curl);
+
+ // cout << chunk.returnMemoryAsString() << endl;
+
+  if (result != CURLE_OK) {
+    cout << "ERROR: " << curl_easy_strerror(result) << endl;
+    return 2;
+  }
+
+  // chunk.memory.push_back('\0');
+
+  curl_easy_cleanup(curl);
+  cout << "Finished succesful" << endl;
+  
+  return 0;
 }
