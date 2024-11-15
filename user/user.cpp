175a022a6aa8e11e1697ea266efdd9779b124855
Szymon Machnik <szymonmachnik07@gmail.com>
2024-11-15 18:20:50 +0100

    Added setUserId function

--- user/user.cpp
+++ user/user.cpp
@@ -80,7 +80,6 @@ void User::loginUser() {
 
 
 
-
 void User::setFirstName() {
   string firstName;
 
@@ -247,21 +246,61 @@ void User::setUserPassword() {
     this->password = password;
 }
 
+void User::setUserId() {
+  if (login.size() == 0) {
+    cout << "ERROR setUserId: Login to get userId doesn't exist" << endl;
+    return;
+  }
+
+  sqlite3 *db;
+  char *errMsg = nullptr;
+
+  ostringstream sql;
+  sql << "SELECT user_id FROM users WHERE login = '" << this->login << "';";
+
+  if (sqlite3_open("sqlite/database.db", &db) != SQLITE_OK) {
+    cerr << "Can not open database: " << sqlite3_errmsg(db) << endl;
+    return;
+  }
+
+  auto callback = [](void *data, int argc, char **argv, char **azColName) -> int {
+    string *result = static_cast<string *>(data);
+    for (int i = 0; i < argc; i++) {
+      *result += string(argv[i]);
+    }
+    
+    return 0;
+  };
+  
+  string result;
+  if (sqlite3_exec(db, sql.str().c_str(), callback, &result, &errMsg) != SQLITE_OK) {
+    cerr << "Error in making db request: " << errMsg << endl;
+    sqlite3_free(errMsg);
+  }
+
+  sqlite3_close(db);
+
+  this->userId = result[0] - '0';
+
+  cout << userId << endl;
+  
+}
 
 void User::registerUser() {
 
   setUserLogin();
   setUserPassword();
-  
-  this->isUserLoged = true;
-  this->balanceInCents = 0;
 
   setFirstName();
   setLastName();
 
-  cout << "Welcome " << firstName << " " << lastName << "!" << endl;
+  this->isUserLoged = true;
+  this->balanceInCents = 0;
 
   saveData();
+  cout << "Welcome " << firstName << " " << lastName << "!" << endl;
+  
+  setUserId();
 }
 
 void User::deposit(int moneyToDepositInCents) {
--- user/user.h
+++ user/user.h
@@ -14,6 +14,9 @@ using namespace std;
 
 class User {
   private:
+
+    int userId;
+
     string password;
     string login; 
     bool isUserLoged;
@@ -31,6 +34,7 @@ class User {
     void setFirstName();
     void setLastName();
 
+    void setUserId();
 
   public:
     User();
