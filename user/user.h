985c91579bd0fefea22324dc5a6b5986ffd9e8a2
Szymon Machnik <szymonmachnik07@gmail.com>
2024-11-19 18:46:22 +0100

    Added user.h comments

--- user/user.h
+++ user/user.h
@@ -15,33 +15,32 @@ using namespace std;
 
 class User {
   private:
-
+    // User data
     int userId;
-
     string password;
     string login; 
     bool isUserLoged;
-
     string firstName;
     string lastName;
 
+    // Balance and limits
     int balanceInCents;
     int balanceAboveWhichUserCantDepositInCents;
 
+    // Manage user data
     void setUserLogin();
     void setUserPassword();
-
-
     void setFirstName();
     void setLastName();
     void setUserId();
-
     void setUserDataDuringLogin();
 
+    // User db
     void setBalanceInDb();
     bool doesLoginExistInDb(string login);
     void insertUserIntoDb();
 
+    // Wallet
     void generateUserWallet();
     bool doesCryptoExistInWallet(string cryptoName);
     void insertCryptoToWallet(string cryptoName, double cryptoAmount, double cryptoPrice, int valueCent);
@@ -50,28 +49,34 @@ class User {
     string setCryptoName();
     double setCryptoAmount();
 
+    // Transaction list
     void generateUserTransactionsList();
     string getCurrentDate();
     void insertTransactionToTransactionsList(string cryptoName, double cryptoAmount, double cryptoPrice, int valueCent, string type);
 
+    // Crypto info
     double returnPriceOfCrypto(string crypto);
     int getCryptoId(string cryptoName);
         
   public:
+    // Constructor
     User();
 
+    // Registration and login
     bool getUserLoginStatus();
     void loginUser();
     void registerUser();
 
+    // Balance
     void deposit(int moneyInCents);
     int getBalanceInCents();
 
+    // Crypto
     void buyCrypto();
     void buyCrypto(string crypto);
 
+    // Displaying
     void displayWallet();
-
     void displayTransactionsList();
 
 };
